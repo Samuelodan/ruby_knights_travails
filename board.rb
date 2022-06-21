6187fb0b575f911e9ff31b9ba02efe8584ecad56
Samuel ODaniels <samodan195@gmail.com>
2022-06-21 20:29:37 +0100

    Write Board#add_adj

--- board.rb
+++ board.rb
@@ -8,6 +8,22 @@ class Board
     @squares = []
   end
 
+  def add_adj(coor)
+    arr = []
+    coor => [x, y]
+
+    arr << [x - 2, y - 1]
+    arr << [x + 2, y - 1]
+    arr << [x - 2, y + 1]
+    arr << [x + 2, y + 1]
+    arr << [x - 1, y + 2]
+    arr << [x - 1, y - 2]
+    arr << [x + 1, y + 2]
+    arr << [x + 1, y - 2]
+
+    arr
+  end
+
   def look_for(coor)
     for square in @squares
       return square if square.coordinate == coor
