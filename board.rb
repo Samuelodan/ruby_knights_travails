dd456434be095ae831c27a7db49e492f4bf639b0
Samuel ODaniels <samodan195@gmail.com>
2022-06-21 19:48:30 +0100

    Write Board#look_for

--- board.rb
+++ board.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
+require_relative './square.rb'
+
 # creates boards
 class Board
   def initialize
     @squares = []
+  end
+
+  def look_for(coor)
+    for square in @squares
+      return square if square.coordinate == coor
+    end
   end
 end
